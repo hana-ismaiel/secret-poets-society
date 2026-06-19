80ad206cf04c973d5a8bb5ddea5c0b4366b18acb
hana-ismaiel <hanaismaiel1@gmail.com>
2026-06-18 21:10:40 -0400

    created basic pages + components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom"
+import NotFound from "./pages/NotFound"
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   )
 }
